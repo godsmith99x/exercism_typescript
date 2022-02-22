3e5942b844c79b7235d4230ac4b86178666b2a55
godsmith <jgodfrey-smith@pm.me>
2022-02-22 13:24:31 -0500

    Completed leap

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,3 +1,9 @@
-export function isLeap() {
-  throw new Error('Remove this statement and implement this function')
+export function isLeap(year: number): boolean {
+  if (year % 4 !== 0) {
+    return false;
+  } else if (year % 100 === 0 && year % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
