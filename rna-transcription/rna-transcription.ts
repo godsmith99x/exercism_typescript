9bb5aca0f8ab8eaf7fb2fd4622a6abbfbf125b37
godsmith <jgodfrey-smith@pm.me>
2022-02-22 14:39:14 -0500

    Completed rna-transcription

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,3 +1,18 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+export function toRna(dna: string): string {
+  return dna
+    .split("")
+    .map((nucleotide: string) => {
+      switch (nucleotide) {
+        case "C":
+          return "G";
+        case "G":
+          return "C";
+        case "A":
+          return "U";
+        case "T":
+          return "A";
+      }
+      throw new Error("Invalid input DNA.");
+    })
+    .join("");
 }
