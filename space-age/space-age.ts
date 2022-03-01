7ef68934ca018a70354bd898ef1d17c70b466475
godsmith <jgodfrey-smith@pm.me>
2022-03-01 16:19:20 -0500

    Completed space-age

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,3 +1,18 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export enum planetRatios {
+  "earth" = 1.0,
+  "mercury" = 0.2408467,
+  "venus" = 0.61519726,
+  "mars" = 1.8808158,
+  "jupiter" = 11.862615,
+  "saturn" = 29.447498,
+  "uranus" = 84.016846,
+  "neptune" = 164.79132,
+}
+
+type pr = keyof typeof planetRatios;
+
+export function age(planet: string, seconds: number): number {
+  let earthYears = seconds / 31557600;
+  let ans = (earthYears / planetRatios[planet as pr]).toFixed(2);
+  return parseFloat(ans);
 }
