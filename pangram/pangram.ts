694226981b37cd31ec6fa9e10b04e9c8a2103c53
godsmith <jgodfrey-smith@pm.me>
2022-03-02 10:43:13 -0500

    Completed pangram

--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -1,3 +1,12 @@
-export function isPangram() {
-  throw new Error('Remove this statement and implement this function')
+export function isPangram(phrase: string): boolean {
+  if (!phrase) {
+    return false;
+  } else {
+    let comparator = new Set("abcdefghijklmnopqrstuvwxyz");
+    let testCase = new Set(phrase.replace(/\s/g, "").toLowerCase());
+
+    let diff = new Set([...comparator].filter((value) => !testCase.has(value)));
+
+    return diff.size === 0;
+  }
 }
