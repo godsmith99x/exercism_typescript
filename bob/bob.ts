b4b003c49f4ef1a426b71f42a97770fe3b8e12ca
godsmith <jgodfrey-smith@pm.me>
2022-03-02 12:38:03 -0500

    Completed bob

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,3 +1,18 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function hey(message: string): string {
+  const answers: string[] = [
+    "Whatever.",
+    "Sure.",
+    "Whoa, chill out!",
+    "Calm down, I know what I'm doing!",
+  ];
+
+  const speech = message.trim();
+
+  if (speech === "") return "Fine. Be that way!";
+
+  const isQuestion: number = speech.endsWith("?") ? 1 : 0;
+  const isShout: number =
+    /[A-Z]+/.test(speech) && speech === speech.toUpperCase() ? 2 : 0;
+
+  return answers[isQuestion + isShout];
 }
